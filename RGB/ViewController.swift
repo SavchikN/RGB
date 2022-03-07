27573b69a6857e34c7dca849eee9a58bd5f2af7a
Nikita Savchik <nikitasavchik50@gmail.com>
2022-03-07 18:01:15 +0100

    correct code

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var colorView: UIView!
-    @IBOutlet var redAlpha: UILabel!
-    @IBOutlet var greenAlpha: UILabel!
-    @IBOutlet var blueAlpha: UILabel!
+    
+    @IBOutlet var redSliderValue: UILabel!
+    @IBOutlet var greenSliderValue: UILabel!
+    @IBOutlet var blueSliderValue: UILabel!
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
@@ -20,35 +22,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         colorView.layer.cornerRadius = 10
         
-        redAlpha.text = String(format: "%.2f", redSlider.value)
-        greenAlpha.text = String(format: "%.2f", greenSlider.value)
-        blueAlpha.text = String(format: "%.2f", blueSlider.value)
+        redSliderValue.text = String(format: "%.2f", redSlider.value)
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
         
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
         
-        colorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1.0
-        )
-    }
-    
-    func changeColor() {
-        colorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1.0
-        )
+        changeColor()
     }
     
     @IBAction func sliderChangedColor() {
         changeColor()
-        redAlpha.text = String(format: "%.2f", redSlider.value)
-        greenAlpha.text = String(format: "%.2f", greenSlider.value)
-        blueAlpha.text = String(format: "%.2f", blueSlider.value)
+        redSliderValue.text = String(format: "%.2f", redSlider.value)
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        )
     }
 }
